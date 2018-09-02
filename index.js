dfd49e79535c41e1bdf691e504cc96da5d5537a8
chihaoyo <chihaoyo@gmail.com>
2018-09-02 17:58:04 +0200

    Upgrade pages.json to sitemap.json; upgrade index.js

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 const fs = require('fs')
-const pages = require('./pages.json')
-const dir = 'p'
+const sitemap = require('./sitemap.json')
 
-pages.forEach(page => {
-  let html = `<!DOCTYPE html>
+function generatePageHTML(dir, docURL) {
+  let depth = dir.split('/')
+  let baseURL = ''
+  for(let i = 0; i < depth.length - 2; i++) {
+    baseURL += '../'
+  }
+  if(!baseURL) {
+    baseURL = './'
+  }
+  return `<!DOCTYPE html>
 <html>
 <head>
   <meta data-n-head="true" charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../styles.css">
+  <link rel="stylesheet" href="${baseURL}styles.css">
 </head>
 <body>
-  <iframe src="${page.docURL}"></iframe>
+  <iframe src="${docURL}"></iframe>
 </body>
 </html>
 `
-  fs.writeFileSync(dir + '/' + page.slug + '.html', html)
-})
+}
+
+function writePage(page, dir, fileName) {
+  fs.writeFileSync(dir + fileName, generatePageHTML(dir, page.docURL))
+}
+
+// write index page
+writePage(sitemap.index, './', 'index.html')
+
+// write pages
+const dir = 'p'
+sitemap.pages.forEach(page => writePage(page, `./${dir}/`, `${page.slug}.html`))
 
 //fs.writeFileSync('old_citizens_after_review.sql', sql)
